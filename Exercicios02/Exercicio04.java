87b4519f0eddaf6762860da22fa6f0e053c48a84
Dannimsz <162757238+Dannimsz@users.noreply.github.com>
2024-04-04 19:10:26 -0300

    Add files via upload

---
+++ Exercicios02/Exercicio04.java
@@ -0,0 +1,30 @@
+package aula09Exercicios;
+import java.util.Scanner;
+
+public class Exercicio04 {
+	public static void main(String[] args) {
+		//Repita o anterior ignorando maiúscula de minúscula 
+		
+		//Validar cidade = "Curitiba"
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digite o nome da sua Cidade: ");
+		String cidade = leitor.next().toUpperCase();
+		
+		//String cidadeCuritiba = "Curitiba";
+		
+		switch (cidade){
+		
+		case "CURITIBA":
+			System.out.println("A cidade é Curitiba!!");
+			break;
+		
+		default:
+			System.out.println("A cidade não é Curitiba");
+			break;
+		}
+		
+		leitor.close();		
+	}
+}
